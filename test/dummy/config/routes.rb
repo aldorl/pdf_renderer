a08226ee7eaa12d055dde426cebddf249aa40554
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-04-22 22:35:28 -0400

    Implement basic functionality of pdf rendering

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get "/home", to: "home#index", as: :home
+  get "/another", to: "home#another", as: :another
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
